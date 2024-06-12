27e8b200d71ac732fb6731b82b5340c2e193351b
Evan Agustian Lukius <71422920+2072017EvanAgustianLukius@users.noreply.github.com>
2024-06-12 11:01:41 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,70 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const sequelize = require('./config/database');
+const UserController = require('./controllers/UserController');
+const ProductController = require('./controllers/ProductController');
+const OrderController = require('./controllers/OrderController');
 
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
         host: 'localhost'
     });
+
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+    server.route([
+        {
+            method: 'POST',
+            path: '/register',
+            handler: UserController.register
+        },
+        {
+            method: 'POST',
+            path: '/login',
+            handler: UserController.login
+        },
+        {
+            method: 'PUT',
+            path: '/update',
+            handler: UserController.update
+        },
+        {
+            method: 'POST',
+            path: '/product',
+            handler: ProductController.addProduct
+        },
+        {
+            method: 'DELETE',
+            path: '/product/{productID}',
+            handler: ProductController.deleteProduct
+        },
+        {
+            method: 'GET',
+            path: '/product/{productID}',
+            handler: ProductController.viewProductDetails
+        },
+        {
+            method: 'POST',
+            path: '/order',
+            handler: OrderController.createOrder
+        },
+        {
+            method: 'PUT',
+            path: '/order',
+            handler: OrderController.updateOrder
+        },
+        {
+            method: 'DELETE',
+            path: '/order',
+            handler: OrderController.cancelOrder
+        }
+    ]);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
